631af419619e7ef9c1a9a457355df000ccbdae41
LucioAlonso <lucioalonso530@gmail.com>
2023-02-03 14:03:44 -0300

    Lucio Alonso correcciones

--- plambda_LucioAlonso/src/com/plambda/Test.java
+++ plambda_LucioAlonso/src/com/plambda/Test.java
@@ -26,13 +26,13 @@ public class Test {
 		casas.add(casa3);
 		
 		//Paso1 Conseguir los m2 totales de las casas del listado
-		System.out.println("M2 totales: " + 
+		System.out.println("[Paso 1] M2 totales: " + 
 				casas.stream()
 				.mapToInt(a -> a.getM2()).sum());
 				
 		//Paso2 Conseguir los m2 totales de las casas del listado 
 		//que tengan un nombre de menos de 4 caracteres
-		System.out.println("\nM2 totales con menos de 4 caracteres: " + 
+		System.out.println("\n[Paso 2] M2 totales con menos de 4 caracteres: " + 
 				casas.stream()
 				.mapToInt(a -> (a.getNombre().length() < 4) ? a.getM2() : 0)
 				.sum());
@@ -40,12 +40,12 @@ public class Test {
 
 		//Paso3 Conseguir un listado de las casa que tengan un salon de m�s de 20 m2
 		/*
-		System.out.println("\nListado de casas con un salon de mas de 20m2: ");
 		casas.stream()
 			.filter(a -> (a.getSalon().getM2() > 20))
 			.forEach(System.out::println);
 		*/
 		//otra forma
+		System.out.println("\n[Paso 3] Listado de casas con un salon de mas de 20m2: ");
 		List<Casa> casas20M2 = casas.stream()
 				.filter(a -> (a.getSalon().getM2() > 20))
 				.collect(Collectors.toList());
@@ -54,33 +54,50 @@ public class Test {
 		
 		
 		//Paso4 Conseguir un listado de Salones con las casas de m�s de una habitaci�n
-		System.out.println("\nListado de Salones con mas de una habitacion: ");
-		casas.stream()
+		System.out.println("\n[Paso 4] Conseguir un listado de Salones con las casas de mas de una habitacion: ");
+		List<Casa> paso4 = casas.stream()
 			.filter(a -> (a.getNumHabitaciones() > 1))
-			.forEach(b -> System.out.println(b.getSalon()));
-	
+			.collect(Collectors.toList());
+		paso4.stream().forEach(System.out::println);
 		
 		
 				
 		//Paso5 Conseguir los m2 de todos los salones de las casas
-		System.out.println("\nLos m2 de todos los salones de las casas: ");
+		/*
 		casas.stream()
 			.forEach(b -> System.out.println(b.getSalon().getM2()));
+		*/
+		System.out.println("\n[Paso 5] Los m2 de todos los salones de las casas: ");
+		List<Salon> paso5 = casas.stream()
+				.map(a -> a.getSalon())
+				.collect(Collectors.toList());
+		paso5.stream().forEach(System.out::println);
 		
 		
 		
 		//Paso6 Imprimir todas las casas
-		System.out.println("\nImprimir todas las casas: ");
+		System.out.println("\n[Paso 6] Imprimir todas las casas: ");
 		casas.stream()
 			.forEach(System.out::println);
 		
 		//Paso7 Conseguir el numero total de habitaciones de las casas de mas de 1 habitaci�n,
 		//      imprimiendo las casas que se han seleccionado.
+		/*
 		System.out.println("\nNumero total de habitaciones de las casas de mas de 1 habitacion imprimiendo las casas que se han seleccionado: ");
 		casas.stream()
 			.filter(a -> (a.getNumHabitaciones() > 1))
 			.forEach(System.out::println);
+			*/
+		//otra forma
 		
+		int contador = casas.stream()
+			.filter(a -> (a.getNumHabitaciones() > 1))
+			.mapToInt(a -> {
+				System.out.println(a);
+				return a.getM2();
+			})
+			.sum();
+
 		
 	}
 
